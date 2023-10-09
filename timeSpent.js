d7cf7e95cfba649f6c6b04d8a1400be5f3bac53c
Tan <tanishamahmud13@gmail.com>
2023-10-08 21:57:09 -0400

    fixed bugs, reduced to one observer

--- timeSpent.js
+++ timeSpent.js
@@ -1,52 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
     let user_time = [
-        { section: "home", startTime: null, timeSpent: 0 },
-        { section: 'about', startTime: null, timeSpent: 0 },
-        { section: 'projects', startTime: null, timeSpent: 0 }, 
-        { section: 'contact', startTime: null, timeSpent: 0 }
+        { section: "home", timeSpent: 0 },
+        { section: 'about', timeSpent: 0 },
+        { section: 'projects', timeSpent: 0 }, 
+        { section: 'contact', timeSpent: 0 }
     ];
     let currentSection = null;
-    let observerTimeoutId;
     updateChart(user_time); 
+    let scrollins = document.querySelectorAll('.scroll-in');
     
     const observer = new IntersectionObserver(entries => {
-        clearTimeout(observerTimeoutId); 
-        observerTimeoutId = setTimeout(() => {
-            entries.forEach(entry => {
-                const sectionName = entry.target.getAttribute('id');
-                if (entry.isIntersecting && currentSection !== sectionName) {
-                    if (currentSection) {
-                        if (currentSection === "home"){
-                            stopTimer(); 
-                        }
-                        else{
-                            recordExit(currentSection);
-                        }
-                    }
-                    currentSection = sectionName;
-                    if (sectionName === "home") {
-                        startTimer(); 
-                    }
-                    else{
-                        recordEntry(sectionName);
+        entries.forEach(entry => {
+            const sectionName = entry.target.getAttribute('id');
+            if (entry.isIntersecting && currentSection !== sectionName) {
+                if (currentSection) {
+                    stopTimer();
+                    if (currentSection === "home"){
+                        scrollins.forEach(scrollin => {
+                            scrollin.classList.remove('appear');
+                        })
                     }
                 }
-                updateChart(user_time); 
-            });
-        }, 1000); 
+                currentSection = sectionName;
+                startTimer(currentSection);
+                if (currentSection === "home"){
+                    scrollins.forEach(scrollin => {
+                        scrollin.classList.add('appear');
+                    })
+                }
+            } 
+        });
     }, { threshold: 0.51 });
         
     document.querySelectorAll('section').forEach(section => {
         observer.observe(section);
     });
 
-    function startTimer(){
+    function startTimer(sectionName){
         let startTime = new Date().getTime(); 
         timerInterval = setInterval( () => {
             const currentTime = new Date().getTime(); 
             const elapsedTime = (currentTime - startTime) / 1000; 
-            const homeIndex = user_time.findIndex(times => times.section === "home");
-            user_time[homeIndex].timeSpent += elapsedTime; 
+            const sectionIndex = user_time.findIndex(times => times.section === sectionName);
+            user_time[sectionIndex].timeSpent += elapsedTime; 
             startTime = currentTime; 
             updateChart(user_time); 
         }, 1000); 
@@ -54,21 +50,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function stopTimer(){
         clearInterval(timerInterval); 
-    }
-
-    function recordEntry(sectionName){
-        const timeIndex = user_time.findIndex(times => times.section === sectionName);
-        user_time[timeIndex].startTime = new Date(); 
-    }
-
-    function recordExit(sectionName){
-        const timeIndex = user_time.findIndex(times => times.section === sectionName);
-        if (user_time[timeIndex].startTime !== null){
-            const time = user_time[timeIndex];
-            time.timeSpent = (new Date() - time.startTime) / 1000;
-            if (time.timeSpent > 0){
-                user_time[timeIndex].timeSpent += time.timeSpent
-            }
-        }
     }
 }); 

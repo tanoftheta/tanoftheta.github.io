930d41f030f37e063445bf006bbad849b7a05adc
Tan <tanishamahmud13@gmail.com>
2024-04-17 18:19:46 -0400

    changed resume button color

--- src/components/About/ResumeView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-
-export const ResumeView = () => {
-    return (
-        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-            Open Resume
-        </a>
-    );
-}
